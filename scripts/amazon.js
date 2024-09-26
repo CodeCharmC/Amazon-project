6c0003a1ba2fb86798c2e59f6cfa42a375a2245a
Moni Akhtar <moni.akhtar1066@gmail.com>
2024-09-26 20:45:52 +0600

    Make add to cart button interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,59 +1,11 @@
+//Step1: Collectt the data;
+/**
+ const products = []
+*/
 
-
-//Step1: Collectt the data
-const products = [
-   {
-      id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-      name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-      rating: {
-         stars: 4.5,
-         count: 87
-      },
-      priceCents: 1090,
-      keywords: [
-         "socks",
-         "sports",
-         "apparel"
-      ]
-   },
-   {
-      id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-      image: "images/products/intermediate-composite-basketball.jpg",
-      name: "Intermediate Size Basketball",
-      rating: {
-         stars: 4,
-         count: 127
-      },
-      priceCents: 2095,
-      keywords: [
-         "sports",
-         "basketballs"
-      ]
-   },
-   {
-      id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-      image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-      name: "Adults Plain Cotton T-Shirt - 2 Pack",
-      rating: {
-        stars: 4.5,
-        count: 56
-      },
-      priceCents: 799,
-      keywords: [
-        "tshirts",
-        "apparel",
-        "mens"
-      ],
-      type: "clothing",
-      sizeChartLink: "images/clothing-size-chart.png"
-   }
-]
-
-
-//Step1: Genarating HTML 
+//Step2: Genarating HTML; 
 let productHtml = "";
-products.forEach( product => {
+products.forEach(product => {
    productHtml += `
          <div class="product-container">
 
@@ -70,7 +22,7 @@ products.forEach( product => {
             <div class="product-rating-container">
                <img 
                   class="product-rating-stars"
-                  src="images/ratings/rating-${product.rating.stars*10}.png"
+                  src="images/ratings/rating-${product.rating.stars * 10}.png"
                >
                <div class="product-rating-count link-primary">
                   ${product.rating.count}
@@ -78,7 +30,7 @@ products.forEach( product => {
             </div>
 
             <div class="product-price">
-               ${(product.priceCents/100).toFixed(2)}
+               ${(product.priceCents / 100).toFixed(2)}
             </div>
 
             <div class="product-quantity-container">
@@ -103,12 +55,40 @@ products.forEach( product => {
                Added
             </div>
 
-            <button class="add-to-cart-button button-primary">
+            <button
+               class="add-to-cart-button button-primary js-add-to-cart-btn"
+               data-product-id= "${product.id}"
+            >
                Add to Cart
             </button>
 
          </div>`
    
-   console.log(products);
-   document.querySelector(".js-products-grid").innerHTML = productHtml
-})
+   document.querySelector(".js-products-grid").innerHTML = productHtml;   
+
+   //Step3: Make the html interactive;
+   document.querySelectorAll(".js-add-to-cart-btn").forEach((button) => {
+      button.addEventListener("click", () => {
+         const productId = button.dataset.productId;
+         console.log(productId)
+
+         let matchingItem;
+         cart.forEach((item) => {            
+            if (productId === item.productId) {
+               matchingItem = item;
+            }
+         });
+
+         if (matchingItem) {
+            matchingItem.quantity += 1;
+         } else {
+            cart.push({
+               productId: productId,
+               quantity: 1
+            });
+         }
+         
+         console.log(cart);
+      });
+   });
+});
