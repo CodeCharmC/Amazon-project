89cd71f7a11a4202658216ae8811667f3a4b224e
Moni Akhtar <moni.akhtar1066@gmail.com>
2024-09-28 23:31:29 +0600

    Genarate html for payment-summary  --View of (MVC)

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -3,7 +3,8 @@ import { ismatchingProduct } from "../../data/products.js";
 import { formateCurrency } from "../utils/money.js";
 import { isdelivery } from "../../data/deliveryOption.js";
 
-export function renderPaymentSummary() {   
+export function renderPaymentSummary() { 
+   let paymentSummaryHtml = "";
    let totalPrice = 0;
    let totalDeliveryCost = 0;
    cart.forEach((cartItem) => {
@@ -34,5 +35,51 @@ export function renderPaymentSummary() {
       };
       return formateCurrency(totalPrice);
    };
+
+   paymentSummaryHtml += `
+      <div class="payment-summary-title">
+         Order Summary
+      </div>
+
+      <div class="payment-summary-row">
+         <div>Items (${calculateCartQuantity()}):</div>
+         <div class="payment-summary-money">
+            $${formateCurrency(totalPrice)}
+         </div>
+      </div>
+
+      <div class="payment-summary-row">
+         <div>Shipping &amp; handling:</div>
+         <div class="payment-summary-money">
+            $${formateCurrency(totalDeliveryCost)}
+         </div>
+      </div>
+
+      <div class="payment-summary-row subtotal-row">
+         <div>Total before tax:</div>
+         <div class="payment-summary-money">
+            $${formateCurrency(totalBeforeTax)}
+         </div>
+      </div>
+
+      <div class="payment-summary-row">
+         <div>Estimated tax (10%):</div>
+         <div class="payment-summary-money">
+            $${formateCurrency(tax)}
+         </div>
+      </div>
+
+      <div class="payment-summary-row total-row">
+         <div>Order total:</div>
+         <div class="payment-summary-money">
+            $${formateCurrency(orderTotal)}
+         </div>
+      </div>
+
+      <button class="place-order-button button-primary">
+         Place your order
+      </button>
+   `
+   document.querySelector(".js-payment-summary").innerHTML = paymentSummaryHtml;
 };
 
