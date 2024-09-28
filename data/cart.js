f0e3bac60d7f9b5e40187afa55467fc19611e405
Moni Akhtar <moni.akhtar1066@gmail.com>
2024-09-28 10:24:07 +0600

    Save data for delivery options

--- data/cart.js
+++ data/cart.js
@@ -1,7 +1,13 @@
 export let cart = JSON.parse(localStorage.getItem("cart")) || [
    {
       productId: "58b4fc92-e98c-42aa-8c55-b6b79996769a",
-      quantity: 1
+      quantity: 1,
+      deliveryOptionsId: "1"
+   },
+   {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 1,
+      deliveryOptionsId: "3"
    }
 ];
 
@@ -48,7 +54,8 @@ export function addToCart(productId) {
    } else {
       cart.push({
          productId,
-         quantity: selectedProductValue
+         quantity: selectedProductValue,
+         deliveryOptionsId: "1"
       });
    };
    saveToLocalStorage();   
@@ -67,7 +74,8 @@ export function updateQuantity(productId, cartQuantity) {
    } else {
       cart.push({
          productId,
-         quantity: cartQuantity
+         quantity: cartQuantity,
+         deliveryOptionsId: "1"
       });
    };
    saveToLocalStorage();
