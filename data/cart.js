93bbcd4ca21ffd98060ba4a6252e489d9c466b2c
Moni Akhtar <moni.akhtar1066@gmail.com>
2024-09-29 19:35:27 +0600

    Re-organize cart using function

--- data/cart.js
+++ data/cart.js
@@ -1,15 +1,23 @@
-export let cart = JSON.parse(localStorage.getItem("cart")) || [
-   {
-      productId: "58b4fc92-e98c-42aa-8c55-b6b79996769a",
-      quantity: 1,
-      deliveryOptionsId: "1"
-   },
-   {
-      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      quantity: 1,
-      deliveryOptionsId: "3"
-   }
-];
+//Procedual Programming
+
+export let cart;
+
+loadCart();
+
+export function loadCart() {
+   cart = JSON.parse(localStorage.getItem("cart")) || [
+      {
+         productId: "58b4fc92-e98c-42aa-8c55-b6b79996769a",
+         quantity: 1,
+         deliveryOptionsId: "1"
+      },
+      {
+         productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+         quantity: 1,
+         deliveryOptionsId: "3"
+      }
+   ];
+};
 
 function ismatching(productId) {
    let matchingItem;
