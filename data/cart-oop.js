e4e0979dfdc9c6a809d7785349257d5daf04f836
Moni Akhtar <moni.akhtar1066@gmail.com>
2024-09-29 20:47:10 +0600

    Create an object 'cart' and run it, by just converting cart of cart.js to an object named cart

---
+++ data/cart-oop.js
@@ -0,0 +1,117 @@
+//OOP : Object Oriented Programming
+const cart = {
+   cartItems: undefined,
+   loadCart() {
+      this.cartItems = JSON.parse(localStorage.getItem("cart-oop")) || [
+         {
+            productId: "58b4fc92-e98c-42aa-8c55-b6b79996769a",
+            quantity: 1,
+            deliveryOptionsId: "1"
+         },
+         {
+            productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            quantity: 1,
+            deliveryOptionsId: "3"
+         }
+      ];
+   },
+   ismatching(productId) {
+      let matchingItem;
+      this.cartItems.forEach((cartItem) => {
+         if (productId === cartItem.productId) {
+            matchingItem = cartItem;
+         }
+      });  
+      return matchingItem;
+   },
+   saveToLocalStorage() {
+      localStorage.setItem("cart-oop", JSON.stringify(this.cartItems));
+   },   
+   deleteCartProduct(productId) {
+      let newCart = [];
+
+      this.cartItems.forEach((cartItem) => {
+         if (cartItem.productId !== productId) {
+            newCart.push(cartItem);
+         };
+      });
+
+      this.cartItems = newCart;
+      this.saveToLocalStorage();
+   },
+   calculateCartQuantity() {
+      let cartQuantity = 0;
+      this.cartItems.forEach((cartItem) => {
+         cartQuantity += cartItem.quantity;
+      });
+      return cartQuantity;
+   },
+   addToCart(productId) {
+      let matchingItem = this.ismatching(productId);
+      if (matchingItem) {
+         matchingItem.quantity += 1;
+      } else {
+         this.cartItems.push({
+            productId,
+            quantity: 1,
+            deliveryOptionsId: "1"
+         });
+      }; 
+
+      /*
+      
+      let selectedProduct = (document.querySelector(`.js-product-quantity-${productId}`));
+      if (!selectedProduct) {
+         console.error(`Product with ID ${productId} not found in the DOM.`);
+         return; // Exit the function if the product isn't found
+      }
+      let selectedProductValue = parseInt(selectedProduct.value);
+
+      if (isNaN(selectedProductValue) || selectedProductValue <= 0) {
+         console.error(`Invalid quantity for product ID ${productId}: ${selectedProduct.value}`);
+         return; // Exit if the quantity is invalid
+      }
+         
+      if (matchingItem) {
+         matchingItem.quantity += selectedProductValue;
+      } else {
+         this.cartItems.push({
+            productId,
+            quantity: selectedProductValue,
+            deliveryOptionsId: "1"
+         });
+      }; */
+      this.saveToLocalStorage();   
+   },
+   updateQuantity(productId, cartQuantity) {
+      let matchingItem = this.ismatching(productId);
+      
+      if (matchingItem) {
+         matchingItem.quantity = cartQuantity;
+      } else {
+         this.cartItems.push({
+            productId,
+            quantity: cartQuantity,
+            deliveryOptionsId: "1"
+         });
+      };
+      this.saveToLocalStorage();
+   },
+   updateDeliveryOptions(productId, deliveryOptionsId) {
+      let matchingItem = this.ismatching(productId);
+      
+      if (matchingItem) {
+         matchingItem.deliveryOptionsId = deliveryOptionsId;
+      } else {
+         this.cartItems.push({
+            productId,
+            quantity: 1,
+            deliveryOptionsId
+         });
+      };
+      saveToLocalStorage();
+   }
+}
+
+cart.loadCart();
+console.log(cart);
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,7 +1,7 @@
 import { renderProductsGrid } from "./amazonHome/productsGrid.js";
 import { renderAmazonHeader } from "./amazonHome/amazonHeader.js";
 import { renderAmazonTitle } from "./amazonHome/amazonPageTitle.js";
-
+import "../data/cart-oop.js";
 
 renderAmazonTitle();
 renderAmazonHeader();
