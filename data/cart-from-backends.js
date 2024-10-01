eb70aeb9612e8c7abc56699de3c9989184ed848b
Moni Akhtar <moni.akhtar1066@gmail.com>
2024-10-01 20:34:38 +0600

    Using Promises instead of callback function

---
+++ data/cart-from-backends.js
@@ -0,0 +1,14 @@
+export let carts = [];
+
+export function loadCarts(functionCall) {
+  const xhr = new XMLHttpRequest();
+
+   xhr.addEventListener("load", () => {
+      console.log(xhr.response);
+      console.log("Cart loaded");
+      functionCall();
+   })
+
+  xhr.open("GET", "https://supersimplebackend.dev/cart");
+  xhr.send();
+};
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,12 +1,40 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
-import { loadProducts } from "../data/products.js";
+//import { loadProducts } from "../data/products.js";
+import { loadProducts } from "../data/products-from-backend.js";
+import { loadCarts } from "../data/cart-from-backends.js";
 
 //import "../data/backend-practic.js";
 
-renderCheckoutHeader();
-loadProducts(() => {   
+new Promise((resolve) => {
+   console.log("Loading products...");
+   loadProducts(() => {
+      console.log("Products loaded");
+      resolve();
+   })
+}).then(() => {
+   return new Promise((resolve) => {
+      console.log("Loading carts...");
+      loadCarts(() => {
+         console.log("Carts loaded");
+         resolve();
+      });
+   });
+}).then(() => {
    renderOrderSummary();
    renderPaymentSummary();
+   renderCheckoutHeader();
 });
+
+//we use Promise and resolve instead of callback functions because they are easier to read and solve nesting.
+
+/*
+renderCheckoutHeader();
+loadProducts(() => {  
+   loadCarts(() => {
+      renderOrderSummary();
+      renderPaymentSummary();
+   });   
+});
+*/
