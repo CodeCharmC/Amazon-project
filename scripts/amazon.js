c7b48a6efe8125d6cb7c77be425cd80f7ada639c
CodeCharmC <moni.akhtar1066@gmail.com>
2024-10-03 22:55:45 +0600

    In amazn home page changing loadProducts with asyncronous await function

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -2,11 +2,24 @@ import { renderProductsGrid } from "./amazonHome/productsGrid.js";
 import { renderAmazonHeader } from "./amazonHome/amazonHeader.js";
 import { renderAmazonTitle } from "./amazonHome/amazonPageTitle.js";
 //import { loadProducts } from "../data/products.js";
-import { loadProducts } from "../data/products-from-backend.js";
+//import { loadProducts } from "../data/products-from-backend.js";
 //import "../data/cart-class.js";
+import { loadProductsFetch } from "../data/products-from-backend.js";
 
+
+renderAmazonTitle();
+renderAmazonHeader();
+async function loadProducts() {
+   await loadProductsFetch();
+   renderProductsGrid();  
+}
+loadProducts();
+
+/**
 renderAmazonTitle();
 renderAmazonHeader();
 loadProducts(() => {
    renderProductsGrid();
 });
+loadProducts();
+ */
